a9b33684310085af779ada337605d1cd4979e452
bibek <bibeksinghthapa@gmail.com>
2018-11-26 14:01:35 -0500

    added controller and page home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pages#home'
+  resources :sessions, only:[:create]
+
+  mount ActionCable.server, at: '/cable'
 end
